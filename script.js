ba55f15f31da9333a592302f0b5394f467cc0d5f
Deligent2 <148380705+Deligent2@users.noreply.github.com>
2023-10-27 01:52:01 +0300

    funaly

---
+++ script.js
@@ -0,0 +1,53 @@
+
+//let a = 'Hello'
+//let b = 'Ivan'
+//console.log(a, b)
+
+//let a = 'lorem ipsum dolor'
+//let b = a.length
+//console.log(b)
+
+//let a = 'lorem ipsum dolor sit amet'
+//let b = a.split(' ') 
+//let c = b[3] + ' ' + b[0] + ' ' + b[1] + ' ' + b[2] + ' ' + b[4]
+//console.log(c)
+
+//let a = 'children'
+//let b = a.slice(0,-1).toUpperCase() + a.slice(-1).toLowerCase()
+//console.log(b)
+
+//let a = 'boss'
+//let b = a[0] + a[3] + a[2] + a[1] + ','
+//console.log(b)
+
+//let a = 'sunday'
+//let b = a.indexOf('d')
+//console.log('index of letter "d" in the string:', b)
+
+//let a = 'hello//'
+//a = a.substring(0,2) + 'ks' +a.substring(4) 
+//console.log(a)
+
+//let a = 'user'
+//let b = 'new'
+//let c = b + ' ' + a.replace('er', '')  
+//console.log(c)
+
+//let a = 'monday'
+//let b = 'day'
+//let c = b + b + a.slice(0, 3) + a.slice(6)
+//console.log(c)
+
+//let a = 'HELLO'
+//let b = a.charAt(0).toLowerCase() + a.slice(1, 2).toUpperCase() + a.slice(2, 3).toLowerCase() + a.slice(3, 4).toUpperCase() +a.slice(4).toLowerCase() 
+//console.log(b)
+
+//let a = 5
+//let b = '400'
+//let c = b.substring(0, 1) + a + b.substring(2)
+//console.log(c)
+
+// let a = 'monkey'
+// let b = 'banana'
+// let c = a.substring(0, 1) + b[1] + b[2] + b[3] +a[3]
+// console.log(c)
